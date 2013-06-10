56eb8597e0ba230413a348182b0a8ec8bfe794e1
Paul Fu <fu.paul@yahoo.com>
2013-06-10 16:56:31 +0800

    Randomized Bkgd and Char

--- UnityProject/Assets/Scripts/BackgroundChanger.cs
+++ UnityProject/Assets/Scripts/BackgroundChanger.cs
@@ -1,27 +1,39 @@
 using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class BackgroundChanger : MonoBehaviour {
 	
-	public GameObject 	mBackgroundSprite;	
-	bool 				mToggle;
+	public GameObject 	m_BackgroundSprite;
+//	public List<string>	m_list;
+	bool 				m_Toggle;
+//	
 	
 	// Use this for initialization
 	void Start () {
-	
-	}
+		
+	}	
 	
 	// Update is called once per frame
 	void Update () {	
 	
 	}
 	
-	void OnSubmit()
-	{
-		Debug.Log("Helo wrold!");
+	void OnSubmit() //On submitting chat
+	{	
+		Debug.Log("heekowskoekw");
 		
-		mToggle = !mToggle;
+		//m_Toggle = !m_Toggle;
 		
-		mBackgroundSprite.SetActive(mToggle);
+		//m_BackgroundSprite.SetActive(m_Toggle);
+		
+		var maxSpriteCount = m_BackgroundSprite.GetComponent<UISprite>().atlas.spriteList.Count; //Grab max sprite count in Atlas
+		
+		var index = Random.Range(0, maxSpriteCount);
+		
+		
+			
+		m_BackgroundSprite.GetComponent<UISprite>().spriteName = m_BackgroundSprite.GetComponent<UISprite>().atlas.spriteList[index].name;
+		
 	}
 }
